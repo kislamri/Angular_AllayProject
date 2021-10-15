471598c01130e47470efaa6958d7846f0f76dda7
kislamri <55755730+kislamri@users.noreply.github.com>
2021-10-15 09:18:33 -0400

    Add files via upload

--- UserController.java
+++ UserController.java
@@ -7,10 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.allayuser.exception.RecordNotFoundException;
@@ -18,6 +22,8 @@ import com.allayuser.model.User;
 import com.allayuser.service.UserService;
 
 @RestController
+
+
 public class UserController {
 	
 	@Autowired
@@ -67,7 +73,7 @@ public class UserController {
 			return new ResponseEntity<>(service.getAllUser(), HttpStatus.OK);
 			
 		 }
-	 @GetMapping("all/{id}")
+	 @GetMapping("/all/{id}")
 	 public ResponseEntity<User>getById(@PathVariable long id) throws RecordNotFoundException{
 		 
 		 Optional<User> user = ((UserService) service).getById(id);
@@ -75,10 +81,32 @@ public class UserController {
 			 return new ResponseEntity<>(user.get(), HttpStatus.OK);
 		 }else {
 			 throw new RecordNotFoundException();
-		 }
+		 } 
 	 }
-		 
 	 
+	@PutMapping("/all/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable("id") int id, @RequestBody User user){
+		Optional<User> userData = service.findById(id);
+		if(userData.isPresent()) {
+			User _user = userData.get();
+			_user.setEmailId(user.getEmailId());
+			_user.setPassword(user.getPassword());
+			_user.setUserName(user.getUserName());
+			return new ResponseEntity<>(service.saveUser(_user), HttpStatus.OK);
+		}
+		
+		else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	
+	//Delete method is not working currently
+	@DeleteMapping("/all/{id}")
+	public ResponseEntity<User> deleteUser(@PathVariable("id") int id){
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
 	
 
-}
+	}
+	
