c52c3f12e1a190311a2b933df784d3e01efcdef0
kislamri <55755730+kislamri@users.noreply.github.com>
2021-10-15 09:18:53 -0400

    Add files via upload

--- UserRepository.java
+++ UserRepository.java
@@ -4,8 +4,11 @@ package com.allayuser.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 
+import com.allayuser.exception.RecordNotFoundException;
 import com.allayuser.model.User;
+
 
 
 public interface UserRepository extends JpaRepository<User, Integer>{
@@ -15,6 +18,9 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	public User findByEmailIdAndPassword(String emailId, String password);
 
 	public User findByEmailId(String emailId);
+	
+
+	
 
 
 	
